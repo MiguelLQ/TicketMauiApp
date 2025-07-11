470b8d49033d5893a4335f7cd55e7b78a9f4033e
MANAGER <miguellizana03@gmail.com>
2025-07-11 10:17:04 -0500

    renovartoken

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,18 +7,21 @@ namespace MauiFirebase
         public App()
         {
             InitializeComponent();
-
+            InitializeAppAsync(); // Inicializar la aplicación de forma asíncrona
+        }
+        private async void InitializeAppAsync()
+        {
             var authService = new FirebaseAuthService();
-
             if (authService.IsLoggedIn())
             {
+                await authService.ObtenerIdTokenSeguroAsync();
                 MainPage = new AppShell(); // ir directo al sistema
             }
             else
             {
-                MainPage = new NavigationPage(new LoginPage());// mostrar login
+                MainPage = new NavigationPage(new LoginPage()); // mostrar login
+
             }
         }
-
     }
 }
